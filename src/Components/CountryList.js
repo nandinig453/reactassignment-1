043359c56b5cb30fe72e6f864e95977573e20307
ratan <ratanupadhyay6835@gmail.com>
2022-06-01 18:18:36 +0530

    Added heading

--- src/Components/CountryList.js
+++ src/Components/CountryList.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import {response} from './'
+import {response} from '../response'
 function CountryList() {
   return (
     <section>
+      <h1>Welcome</h1>
       <ul className="card country-list" data-testid="countryList">
         <li key="india" className="pa-10 pl-20">India</li>
       </ul>
